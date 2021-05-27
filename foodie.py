de5855406ddcce21f73ccbaf38ecd2c1d56e3ee4
jj-jenny <78203310+jj-jenny@users.noreply.github.com>
2021-05-27 16:24:10 +0800

    Update foodie.py

--- foodie.py
+++ foodie.py
@@ -2,12 +2,12 @@ import telebot
 
 bot = telebot.TeleBot("1754559930:AAGk8J4IbywhOE-RfH8UVN-fWXl9I-zKJ7E")
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+	bot.reply_to(message, "Hello, what would you like to eat today?")
+	
+@bot.message_handler(func=lambda message: True)
+def breathe_all(message):
+	bot.reply_to(message, "I love Food!")
 
 bot.polling()
