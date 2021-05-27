a1abbdac029bfb5caca377b47452bceb2a376084
jj-jenny <78203310+jj-jenny@users.noreply.github.com>
2021-05-27 17:07:52 +0800

    Update foodie.py

--- foodie.py
+++ foodie.py
@@ -4,10 +4,18 @@ bot = telebot.TeleBot("1754559930:AAGk8J4IbywhOE-RfH8UVN-fWXl9I-zKJ7E")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Hello, what would you like to eat today?")
+	bot.reply_to(message, "Hello, what would you like to do today?"
+                     "Try the /getFood command for food recommendations or /splitBill command")
+	bot.send_message(chat_id=update.message.chat_id,
+                text=msg, reply_markup=reply_kb_markup)
 	
-@bot.message_handler(func=lambda message: True)
-def breathe_all(message):
-	bot.reply_to(message, "I love Food!")
+
+@bot.message_handler(commands=['getFood'])
+def get_recommendation(message):
+        bot.reply_to(message, "Please select your region or enter postal code for nearby food recommendations.")
+
+@bot.message_handler(commands=['splitBill'])
+def split_bill(message):
+        bot.reply_to(message, "How many people are splitting the bill?")
 
 bot.polling()
